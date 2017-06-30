2a9c5ff4cca410706a1630c6656972160e8ec744
Ruslan Pedora <ruslan.pedora.gr@gmail.com>
2017-06-30 19:56:44 +0300

    ids refactoring

--- cropper.js
+++ cropper.js
@@ -16,64 +16,48 @@ var canvasTop, canvasLeft;
 
 //document elements
 //----------------------------------------------------------------
-var elementMainCanvas     = document.getElementById( 'main-canvas' );
-var elementSaveCanvas     = document.getElementById( 'save-canvas' );
-var elementPaintContainer = document.getElementById( 'paint-container' );
-var elementSwitchBar  = document.getElementById( 'inner-mode-bar' );
-var elementImageSrc   = document.getElementById( 'image-src' );
-var elementFileSrc    = document.getElementById( 'file-src' );
-var elementURLSrc     = document.getElementById( 'url-src' );
-var elementImgBufer   = document.getElementById( 'img-buffer' );
-var elementTracker    = document.getElementById( 'tracker' );
-var elementImageSelector = document.getElementById( 'image-selector' );
-var elementGlass         = document.getElementById( 'glass' );
-var elementBody          = document.getElementById( 'glass' );
+var mainCanvasEl      = document.getElementById( 'main-canvas' );
+var saveCanvasEl      = document.getElementById( 'save-canvas' );
+var fullImageCanvasEl = document.getElementById( 'full-image-canvas' );
+
+var imgBuferEl = document.getElementById( 'img-buffer' );
+var imgFullEl = document.getElementById( 'full-img-buffer' );
+
+var cropAreaEl = document.getElementById( 'crop-area' );
+var glassEl         = document.getElementById( 'glass' );
 
 
-var elementBrowseButton  = document.getElementById( 'file-browse' );
-var elementCancelButton  = document.getElementById( 'cancel-button' );
-var elementReplaceButton = document.getElementById( 'replace-button' );
-var elementSaveButton    = document.getElementById( 'save-button' );
-var elementServLink      = document.getElementById( 'serv-link' );
+var paintContainerEl = document.getElementById( 'paint-container' );
+var switchBarEl  = document.getElementById( 'inner-mode-bar' );
+var imagePathEl   = document.getElementById( 'image-path' );
+var fileSrcEl    = document.getElementById( 'file-src' );
+
+var trackerEl    = document.getElementById( 'tracker' );
 
 
-var scrModeSelection = document.getElementsByName( 'srcMode' );
+var browseButtonEl  = document.getElementById( 'file-browse' );
+var cancelButtonEl  = document.getElementById( 'cancel-button' );
+var replaceButtonEl = document.getElementById( 'replace-button' );
+var saveButtonEl    = document.getElementById( 'save-button' );
+var servLinkEl      = document.getElementById( 'serv-link' );
+
+
+var scrModeSelectionEls = document.getElementsByName( 'src-selection-mode' );
 
 
 //event handlers
-//----------------------------------------------------------------
-document.addEventListener( 'paste', function ( evt ) {
-  console.log(evt.clipboardData.getData('text/plain'));
-});
-
 var srcModeHandler = function( event ) {
-	if ( event.target.value == 'remote' ) {
-		elementBrowseButton.style.display = 'none';
-		elementURLSrc.style.display = 'initial';
-	} else {
-		elementBrowseButton.style.display = 'initial';
-		elementURLSrc.style.display = 'none';
-	}
+
+	browseButtonEl.style.display = event.target.value === 'remote' ? 'none' : 'initial';
 }
 
-for ( var i = 0; i < scrModeSelection.length; i++ ) {
-	scrModeSelection[ i ].onclick = srcModeHandler;
+for ( var i = 0; i < scrModeSelectionEls.length; i++ ) {
+	scrModeSelectionEls[ i ].onclick = srcModeHandler;
 }
 //----------------------------------------------------------------
-elementBrowseButton.onclick = function() {
-}
-//----------------------------------------------------------------
-elementURLSrc.onclick = function() {
-	var fileName;
-
-	if ( true  ) {
-		cropSwticherOn();
-		elementImageSrc.value = fileName;
-	}	
-}
-//----------------------------------------------------------------
-elementFileSrc.onchange = function( event ) {	
+fileSrcEl.onchange = function( event ) {	
 	var fReader = new FileReader();
+	var fileNameShort = event.target.name;
 
 	fReader.readAsDataURL( event.target.files[ 0 ]);
 
@@ -84,7 +68,7 @@ elementFileSrc.onchange = function( event ) {
 	}
 }
 //----------------------------------------------------------------
-elementGlass.onmousedown = function( event ) {
+glassEl.onmousedown = function( event ) {
 
 	if( !cropModeActivated) {
 		return;
@@ -96,23 +80,23 @@ elementGlass.onmousedown = function( event ) {
 	xEnd = xStart;
 	yEnd = yStart;
 
-	canvasTop  = elementMainCanvas.offsetTop + elementPaintContainer.offsetTop;
-	canvasLeft = elementMainCanvas.offsetLeft + elementPaintContainer.offsetLeft;
+	canvasTop  = paintContainerEl.offsetTop;
+	canvasLeft = paintContainerEl.offsetLeft;
 
 	maxymizeGlass();
 
-	elementImageSelector.style.display = 'initial';
+	cropAreaEl.style.display = 'initial';
 	rednerImageSelector( xStart, yStart, xEnd, yEnd )
 
 	refreshTracker();
 }
 //----------------------------------------------------------------
-elementGlass.onmousemove = function( event ) {		
+glassEl.onmousemove = function( event ) {		
 	var rect;
 
 	if ( markup ) {
-		xEnd = Math.min( Math.max( event.offsetX - canvasLeft, 0 ), elementMainCanvas.clientWidth );
-		yEnd = Math.min( Math.max( event.offsetY - canvasTop, 0 ), elementMainCanvas.clientHeight );
+		xEnd = Math.min( Math.max( event.offsetX - canvasLeft, 0 ), mainCanvasEl.clientWidth );
+		yEnd = Math.min( Math.max( event.offsetY - canvasTop, 0 ), mainCanvasEl.clientHeight );
 
 		rednerImageSelector( xStart, yStart, xEnd, yEnd );
 		refreshTracker();
@@ -120,20 +104,20 @@ elementGlass.onmousemove = function( event ) {
 		
 }
 //----------------------------------------------------------------
-elementGlass.onmouseup = function( event ) {
+glassEl.onmouseup = function( event ) {
 
 
 	if ( markup ) {
-		var drawingInputContext = elementMainCanvas.getContext( '2d' );
-		var drawingOutputContext = elementSaveCanvas.getContext( '2d' );
+		var drawingInputContext = mainCanvasEl.getContext( '2d' );
+		var drawingOutputContext;
 		var rect;
-		var selectedArea;
+		var areaToCopy;
 
 		markup = false;
-		xEnd = Math.min( Math.max( event.offsetX - canvasLeft, 0 ), elementMainCanvas.clientWidth );
-		yEnd = Math.min( Math.max( event.offsetY - canvasTop, 0 ), elementMainCanvas.clientHeight );
+		xEnd = Math.min( Math.max( event.offsetX - canvasLeft, 0 ), mainCanvasEl.clientWidth );
+		yEnd = Math.min( Math.max( event.offsetY - canvasTop, 0 ), mainCanvasEl.clientHeight );
 
-		elementImageSelector.style.display = 'none';
+		cropAreaEl.style.display = 'none';
 		minimizeGlass();
 		refreshTracker();
 
@@ -143,46 +127,52 @@ elementGlass.onmouseup = function( event ) {
 			return;
 		}
 		
-		selectedArea = drawingInputContext.getImageData( rect.x, rect.y, rect.width, rect.height );
+		areaToCopy = drawingInputContext.getImageData( rect.x, rect.y, rect.width, rect.height );
 
-		drawingOutputContext.clearRect( 0, 0, elementSaveCanvas.clientWidth , elementSaveCanvas.clientHeight );
-		drawingOutputContext.putImageData( selectedArea, 0, 0 );		
+		saveCanvasEl.style.width = rect.width + 'px';
+		saveCanvasEl.style.height = rect.height + 'px';
+		saveCanvasEl.setAttribute( 'width', rect.width );
+		saveCanvasEl.setAttribute( 'height', rect.height );
+
+		drawingOutputContext = saveCanvasEl.getContext( '2d' );		
+		drawingOutputContext.clearRect( 0, 0, saveCanvasEl.clientWidth , saveCanvasEl.clientHeight );
+		drawingOutputContext.putImageData( areaToCopy, 0, 0 );		
 
 	}
 	
 	refreshTracker();
 }
 //----------------------------------------------------------------
-elementGlass.onmouseout = function( event ) {
+glassEl.onmouseout = function( event ) {
 	
 	markup = false;
-	elementImageSelector.style.display = 'none';
+	cropAreaEl.style.display = 'none';
 
 	minimizeGlass();
 	refreshTracker();
 }
 //----------------------------------------------------------------
-elementCancelButton.onclick = function() {
-	var drawingOuputContext = elementSaveCanvas.getContext( '2d' );
+cancelButtonEl.onclick = function() {
+	var drawingOuputContext = saveCanvasEl.getContext( '2d' );
 	
-	drawingOuputContext.clearRect( 0, 0, elementSaveCanvas.clientWidth , elementSaveCanvas.clientHeight );
+	drawingOuputContext.clearRect( 0, 0, saveCanvasEl.clientWidth , saveCanvasEl.clientHeight );
 }
 //----------------------------------------------------------------
-elementReplaceButton.onclick = function() {
-	var drawingOutputContext = elementMainCanvas.getContext( '2d' );
-	var drawingInputContext = elementSaveCanvas.getContext( '2d' );
-	var selectedArea = drawingInputContext.getImageData( 0, 0, elementSaveCanvas.clientWidth, elementSaveCanvas.clientHeight );
+replaceButtonEl.onclick = function() {
+	var drawingOutputContext = mainCanvasEl.getContext( '2d' );
+	var drawingInputContext = saveCanvasEl.getContext( '2d' );
+	var selectedArea = drawingInputContext.getImageData( 0, 0, saveCanvasEl.clientWidth, saveCanvasEl.clientHeight );
 	
 	drawingOutputContext.putImageData( selectedArea, 0, 0 );		
 }
 //----------------------------------------------------------------
-elementSaveButton.onclick = function() {
-	var dataURL = elementSaveCanvas.toDataURL( 'C:\\Development\\Projects\\cropper\\1.jpg' );
-	console.log( dataURL);
+saveButtonEl.onclick = function() {
+	var dataURL = saveCanvasEl.toDataURL( 'images/png' );
 
-	elementServLink.download = 'cropped-image.png';
-	elementServLink.href = dataURL.replace("image/png", "image/octet-stream");
-	elementServLink.click();
+	console.log( dataURL);
+	servLinkEl.download = 'cropped-image.png';
+	servLinkEl.href = dataURL.replace("image/png", "image/octet-stream");
+	servLinkEl.click();
 }
 
 
@@ -190,69 +180,75 @@ elementSaveButton.onclick = function() {
 //----------------------------------------------------------------
 function maxymizeGlass() {
 
-	elementGlass.style.position = 'fixed';
-	elementGlass.style.width    = '100vw';
-	elementGlass.style.height   = '100vh';
+	glassEl.style.position = 'fixed';
+	glassEl.style.width    = '100vw';
+	glassEl.style.height   = '100vh';
 }
 //----------------------------------------------------------------
 function minimizeGlass() {
 
-	elementGlass.style.position = 'absolute';
-	elementGlass.style.width    = elementMainCanvas.clientWidth + 'px';
-	elementGlass.style.height   = elementMainCanvas.clientHeight + 'px';	
+	glassEl.style.position = 'absolute';
+	glassEl.style.width    = mainCanvasEl.clientWidth + 'px';
+	glassEl.style.height   = mainCanvasEl.clientHeight + 'px';	
 }
 //----------------------------------------------------------------
 function rednerImageSelector( xStart, yStart, xEnd, yEnd ) {
 	var	rect = detectSelectedArea( xStart, yStart, xEnd, yEnd );
 
-	elementImageSelector.style.left  = rect.x + 'px';
-	elementImageSelector.style.width = rect.width + 'px';
-	elementImageSelector.style.top    = rect.y + 'px';
-	elementImageSelector.style.height = rect.height + 'px';
+	cropAreaEl.style.left  = rect.x + 'px';
+	cropAreaEl.style.width = rect.width + 'px';
+	cropAreaEl.style.top    = rect.y + 'px';
+	cropAreaEl.style.height = rect.height + 'px';
 }
 //----------------------------------------------------------------
 function detectSelectedArea( xStart, yStart, xEnd, yEnd ) {
 	var rectangle = {};
 
 	if ( xStart < xEnd ) {
-		rectangle.x     = Math.max( xStart, 1 ) + elementMainCanvas.offsetLeft;
+		rectangle.x     = Math.max( xStart, 1 ) + mainCanvasEl.offsetLeft;
 		rectangle.width = xEnd - xStart;
 	} else {
-		rectangle.x     = Math.max( xEnd, 1 ) + elementMainCanvas.offsetLeft;			
+		rectangle.x     = Math.max( xEnd, 1 ) + mainCanvasEl.offsetLeft;			
 		rectangle.width = xStart - xEnd;			
 	}		
-	rectangle.width = Math.min( rectangle.width, elementMainCanvas.clientWidth - rectangle.x - 1 );
+	rectangle.width = Math.min( rectangle.width, mainCanvasEl.clientWidth - rectangle.x - 1 );
 
 	if ( yStart < yEnd ) {
-		rectangle.y      = Math.max( yStart, 1 ) + elementMainCanvas.offsetTop;
+		rectangle.y      = Math.max( yStart, 1 ) + mainCanvasEl.offsetTop;
 		rectangle.height = yEnd - yStart;
 	} else {
-		rectangle.y      = Math.max( yEnd, 1 ) + elementMainCanvas.offsetTop;
+		rectangle.y      = Math.max( yEnd, 1 ) + mainCanvasEl.offsetTop;
 		rectangle.height = yStart - yEnd;
 	}
-	rectangle.height = Math.min( rectangle.height, elementMainCanvas.clientHeight - rectangle.y - 1 );
+	rectangle.height = Math.min( rectangle.height, mainCanvasEl.clientHeight - rectangle.y - 1 );
 
 	return rectangle;
 }
 //----------------------------------------------------------------
 function cropSwticherOn() {
-	elementSwitchBar.style.width = '100%';
-	elementGlass.style.cursor = 'crosshair';
+	switchBarEl.style.width = '100%';
+	glassEl.style.cursor = 'crosshair';
 	cropModeActivated= true;	
 }
 //----------------------------------------------------------------
 function cropSwticherOf() {
-	elementSwitchBar.style.width = SWITCHER_RADIUS + 'px';
+	switchBarEl.style.width = SWITCHER_RADIUS + 'px';
 }
 //----------------------------------------------------------------
 function drawImage( fileName ) {
-	var drawingContext = elementMainCanvas.getContext( '2d' );
-	elementImgBufer.src = fileName;
+	var drawingContext = mainCanvasEl.getContext( '2d' );
+	imgBuferEl.src = fileName;
 	
-	elementImgBufer.onload = function() {
-		elementImageSrc.value = fileName;
-		drawingContext.clearRect( 0, 0, elementMainCanvas.clientWidth , elementMainCanvas.clientHeight );
-		drawingContext.drawImage( elementImgBufer, 0, 0, elementImgBufer.clientWidth, elementImgBufer.clientHeight );
+	imgBuferEl.onload = function() {
+		imagePathEl.value = fileName;
+		drawingContext.clearRect( 0, 0, mainCanvasEl.clientWidth , mainCanvasEl.clientHeight );
+
+		mainCanvasEl.style.width = imgBuferEl.clientWidth + 'px';
+		mainCanvasEl.style.height = imgBuferEl.clientHeight + 'px';
+		mainCanvasEl.setAttribute( 'width', imgBuferEl.clientWidth );
+		mainCanvasEl.setAttribute( 'height', imgBuferEl.clientHeight );
+
+		drawingContext.drawImage( imgBuferEl, 0, 0 );
 
 		cropSwticherOn();
 	}; 
@@ -261,9 +257,9 @@ function drawImage( fileName ) {
 function refreshTracker() {
 	if ( markup ) {
 		var rect = detectSelectedArea( xStart, yStart, xEnd, yEnd );
-		elementTracker.innerText = '' + rect.width + ' x ' + rect.height + 'px' + ' x  = ' + rect.x + ' y  = ' + rect.y;
+		trackerEl.innerText = '   ' + rect.x + ',' + rect.y + ' --> ' + rect.width + ' x ' + rect.height + 'px';
 	} else {
-		elementTracker.innerText = '';
+		trackerEl.innerText = '';
 	}	
 }	
 //----------------------------------------------------------------
